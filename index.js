926d74d5439fa2a1596816172162de0d659d9ef7
Dave <daveg7lee@gmail.com>
2021-05-17 20:18:13 +0900

    Nodemon

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import fastify from 'fastify';
 
-const app = fastify();
+const app = fastify({ logger: true });
+
+app.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
 
 const start = async () => {
   try {
     await app.listen(3000);
   } catch (err) {
+    console.log(err);
     app.log.error(err);
   }
 };
